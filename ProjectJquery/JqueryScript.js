118d03a5ff8b3396d7c7bf5a6ef4a10caf7a7015
MathijsBesten <m88_db@hotmail.com>
2016-04-20 13:03:41 +0200

    made a jquery test function

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,12 +1,15 @@
 ﻿$(document).ready(function () {
-    var allSubs = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd,ee,ff,gg,hh,ii,jj";
     $("#11").click(function () {
-        console.log(["1.1 ingedrukt"]);
+        var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
+        for (var integer = 0; integer < allSubs.length; integer++) {
+            $(allSubs[integer]).css({ "background-color": "transparant" });
+        }
+        var list;
         var classes = $(this).attr("class");
-        alert(classes);
+        list = classes.split(/ /g);
+
+        for (var int = 0; int < list.length; int++) {
+            $("#" + list[int]).css({ "background-color": "yellow" });
+        }
     });
-
-
-
-
 });
