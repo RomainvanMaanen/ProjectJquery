b85f02eb189eb3027405bd466cf73c3622b99b59
MathijsBesten <m88_db@hotmail.com>
2016-04-20 13:43:27 +0200

    added main selected to the jquery function

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,8 +1,12 @@
 ﻿$(document).ready(function () {
     $(".clickable").click(function () {
         var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
+        var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38,39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
         for (var integer = 0; integer < allSubs.length; integer++) {
             $(allSubs[integer]).css({ "background-color": "darkgray" });
+        }
+        for (var integer = 0; integer < allMain.length; integer++) {
+            $("#" + allMain[integer]).css({ "background-color": "darkgray" });
         }
         var list;
         var classes = $(this).attr("class");
@@ -10,5 +14,6 @@
         for (var int = 0; int < list.length; int++) {
             $("#" + list[int]).css({ "background-color": "green" });
         }
+        $(this).css({ "background-color": "greenyellow" });
     });
 });
