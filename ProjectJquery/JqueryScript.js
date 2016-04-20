a8a501c88780f33d67d5392d1823dab2f44f7468
MathijsBesten <m88_db@hotmail.com>
2016-04-20 12:21:33 +0200

    removed dots  in id's, because is a dot is a escape character in jquery

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,3 +1,12 @@
 ﻿$(document).ready(function () {
+    var allSubs = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd,ee,ff,gg,hh,ii,jj";
+    $("#11").click(function () {
+        console.log(["1.1 ingedrukt"]);
+        var classes = $(this).attr("class");
+        alert(classes);
+    });
+
+
+
 
 });
