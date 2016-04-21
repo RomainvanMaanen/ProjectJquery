d81f125f264ea0315d52c4908cde7ff5475f0a0a
RomainvanMaanen <ro.vanmaanen@gmail.com>
2016-04-21 12:12:25 +0200

    merge

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -3,10 +3,10 @@
         var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
         var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38,39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
         for (var integer = 0; integer < allSubs.length; integer++) {
-             $(allSubs[integer]).css({ "background-color": "darkgray"});
+            $(allSubs[integer]).css({ "background-color": "transparent" });
         }
         for (var integer = 0; integer < allMain.length; integer++) {
-            $("#" + allMain[integer]).css({ "background-color": "darkgray" });
+            $("#" + allMain[integer]).css({ "background-color": "transparent" });
         }
         var list;
         var classes = $(this).attr("class");
