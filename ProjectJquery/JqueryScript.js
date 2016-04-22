0130cc2cd699427004d4b0770b8724aebb9d0a43
RomainvanMaanen <ro.vanmaanen@gmail.com>
2016-04-22 14:58:50 +0200

    best i can do xD

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $(".clickable").click(function () {
         var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
-        var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38,39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
+        var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
         for (var integer = 0; integer < allSubs.length; integer++) {
             $(allSubs[integer]).css({ "background-color": "transparent" });
         }
