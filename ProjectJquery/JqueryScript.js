e1fd23a0a88aafc3d1f2cf919e402f64d6cb543d
RomainvanMaanen <ro.vanmaanen@gmail.com>
2016-04-25 16:25:24 +0200

    Pop up info website

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,31 +1,47 @@
-﻿$(document).ready(function () {
-    $(".clickable").click(function () {
-        var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
-        var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
+﻿
+    $(document).ready(function () {
+        $('#popup_background').fadeIn('fast');
+        $('#popup_box').fadeIn('fast');
 
-        if ($(this).css('backgroundColor') == "rgb(221, 56, 90)") {
-            $(".pmain").css({ "background-Color": "transparent" });
-            $(".pmain").css({ "background": "transparent" });
-            console.log("blabla")
-        }
-        else {
+        $(this).click(function () {
+            $('#popup_background').fadeOut('slow');
+            $('#popup_box').fadeOut('slow');
+            $("#Information_website").fadeIn('slow');
+        });
+        $("#Information_website").click(function () {
+            $('#popup_background').fadeIn('slow');
+            $('#popup_box').fadeIn('slow');
+            $("#Information_website").fadeOut('slow')
+            return false;
+        });
 
-            for (var integer = 0; integer < allSubs.length; integer++) {
-                $(allSubs[integer]).css({ "background-color": "transparent" });
+        $(".clickable").click(function () {
+            var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
+            var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
+
+            if ($(this).css('backgroundColor') == "rgb(221, 56, 90)") {
+                $(".pmain").css({ "background-Color": "transparent" });
+                $(".pmain").css({ "background": "transparent" });
+                console.log("blabla")
             }
-            for (var integer = 0; integer < allMain.length; integer++) {
-                $("#" + allMain[integer]).css({ "background-color": "transparent" });
+            else {
+
+                for (var integer = 0; integer < allSubs.length; integer++) {
+                    $(allSubs[integer]).css({ "background-color": "transparent" });
+                }
+                for (var integer = 0; integer < allMain.length; integer++) {
+                    $("#" + allMain[integer]).css({ "background-color": "transparent" });
+                }
+                var list;
+                var classes = $(this).attr("class");
+                list = classes.split(/ /g);
+                for (var int = 0; int < list.length; int++) {
+                    $("#" + list[int]).css({
+                        "transition": "background-color 0.5s ease",
+                        "background-color": "#dd385a"
+                    });
+                }
+                $(this).css({ "background-color": "rgb(221, 56, 90)" });
             }
-            var list;
-            var classes = $(this).attr("class");
-            list = classes.split(/ /g);
-            for (var int = 0; int < list.length; int++) {
-                $("#" + list[int]).css({
-                    "transition": "background-color 0.5s ease",
-                    "background-color": "#dd385a"
-                });
-            }
-            $(this).css({ "background-color": "rgb(221, 56, 90)" });
-        }
+        });
     });
-});
