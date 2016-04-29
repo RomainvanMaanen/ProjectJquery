89c96576dbf6454d802136a0e676517ecd4a00e3
MathijsBesten <m88_db@hotmail.com>
2016-04-29 18:48:10 +0200

    version 1.1

--- ProjectJquery/JqueryScript.js
+++ ProjectJquery/JqueryScript.js
@@ -1,5 +1,4 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     var allSubs = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd, ee, ff, gg, hh, ii, jj];
     var allMain = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 31, 32, 33, 34, 35, 36, 37, 38, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
     var timeout = 0;
